13ea7695a406166efb48b7be592390c3b143290b
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-14 10:19:57 -0500

    Resampling particlles

---
+++ localization/mcl_lab/resampling.cpp
@@ -0,0 +1,24 @@
+//One out of many possible solutions
+#include <iostream>
+
+using namespace std;
+
+double w[] = { 0.6, 1.2, 2.4, 0.6, 1.2 };
+double sum = 0;
+
+void ComputeProb(double w[], int n)
+{
+    for (int i = 0; i < n; i++) {
+        sum = sum + w[i];
+    }
+    for (int j = 0; j < n; j++) {
+        w[j] = w[j] / sum;
+        cout << "P" << j + 1 << "=" << w[j] << endl;
+    }
+}
+
+int main()
+{
+    ComputeProb(w, sizeof(w) / sizeof(w[0]));
+    return 0;
+}
