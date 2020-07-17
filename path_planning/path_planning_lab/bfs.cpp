932d193cca1b54b912dd75983ea98111566476d0
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-17 14:40:16 -0500

    Path search BFS algorithm implementation

--- path_planning/path_planning_lab/bfs.cpp
+++ path_planning/path_planning_lab/bfs.cpp
@@ -48,21 +48,82 @@ void print2DVector(T Vec)
     }
 }
 
+// Search function which generates the expansions
+void search(Map map, Planner planner)
+{
+    // Create a closed 2 array filled with 0s and first element 1
+    vector<vector<int> > closed(map.mapHeight, vector<int>(map.mapWidth));
+    closed[planner.start[0]][planner.start[1]] = 1;
+
+    // Defined the triplet values
+    int x = planner.start[0];
+    int y = planner.start[1];
+    int g = 0;
+
+    // Store the expansions
+    vector<vector<int> > open;
+    open.push_back({ g, x, y });
+
+    // Flags
+    bool found = false;
+    bool resign = false;
+
+    int x2;
+    int y2;
+
+    // While I am still searching for the goal and the problem is solvable
+    while (!found && !resign) {
+        // Resign if no values in the open list and you can't expand anymore
+        if (open.size() == 0) {
+            resign = true;
+            cout << "Failed to reach a goal" << endl;
+        }
+        // Keep expanding
+        else {
+            // Remove triplets from the open list
+            sort(open.begin(), open.end());
+            reverse(open.begin(), open.end());
+            vector<int> next;
+            // Stored the poped value into next
+            next = open.back();
+            open.pop_back();
+
+            x = next[1];
+            y = next[2];
+            g = next[0];
+            
+            // Check if we reached the goal:
+            if (x == planner.goal[0] && y == planner.goal[1]) {
+                found = true;
+                cout << "[" << g << ", " << x << ", " << y << "]" << endl;
+            }
+
+            //else expand new elements
+            else {
+                for (int i = 0; i < planner.movements.size(); i++) {
+                    x2 = x + planner.movements[i][0];
+                    y2 = y + planner.movements[i][1];
+                    if (x2 >= 0 && x2 < map.grid.size() && y2 >= 0 && y2 < map.grid[0].size()) {
+                        if (closed[x2][y2] == 0 and map.grid[x2][y2] == 0) {
+                            int g2 = g + planner.cost;
+                            open.push_back({ g2, x2, y2 });
+                            closed[x2][y2] = 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
     // Instantiate map and planner objects
     Map map;
     Planner planner;
 
-    // Print classes variables
-    cout << "Map:" << endl;
-    print2DVector(map.grid);
-    cout << "Start: " << planner.start[0] << " , " << planner.start[1] << endl;
-    cout << "Goal: " << planner.goal[0] << " , " << planner.goal[1] << endl;
-    cout << "Cost: " << planner.cost << endl;
-    cout << "Robot Movements: " << planner.movements_arrows[0] << " , " << planner.movements_arrows[1] << " , " << planner.movements_arrows[2] << " , " << planner.movements_arrows[3] << endl;
-    cout << "Delta:" << endl;
-    print2DVector(planner.movements);
+    // Search for the expansions
+    search(map, planner);
 
     return 0;
 }
