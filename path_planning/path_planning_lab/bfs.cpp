30dc39db3ed1d57be619525fda6960efb3d132da
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-17 14:45:11 -0500

    Added expansion list to see how the algorithm search

--- path_planning/path_planning_lab/bfs.cpp
+++ path_planning/path_planning_lab/bfs.cpp
@@ -48,12 +48,15 @@ void print2DVector(T Vec)
     }
 }
 
-// Search function which generates the expansions
+// Search function will generate the expansions
 void search(Map map, Planner planner)
 {
     // Create a closed 2 array filled with 0s and first element 1
     vector<vector<int> > closed(map.mapHeight, vector<int>(map.mapWidth));
     closed[planner.start[0]][planner.start[1]] = 1;
+
+    // Create expand array filled with -1
+    vector<vector<int> > expand(map.mapHeight, vector<int>(map.mapWidth, -1));
 
     // Defined the triplet values
     int x = planner.start[0];
@@ -64,9 +67,10 @@ void search(Map map, Planner planner)
     vector<vector<int> > open;
     open.push_back({ g, x, y });
 
-    // Flags
+    // Flags and counters
     bool found = false;
     bool resign = false;
+    int count = 0;
 
     int x2;
     int y2;
@@ -91,11 +95,15 @@ void search(Map map, Planner planner)
             x = next[1];
             y = next[2];
             g = next[0];
+
+            // Fill the expand vectors with count
+            expand[x][y] = count;
+            count += 1;
             
             // Check if we reached the goal:
             if (x == planner.goal[0] && y == planner.goal[1]) {
                 found = true;
-                cout << "[" << g << ", " << x << ", " << y << "]" << endl;
+                //cout << "[" << g << ", " << x << ", " << y << "]" << endl;
             }
 
             //else expand new elements
@@ -114,6 +122,8 @@ void search(Map map, Planner planner)
             }
         }
     }
+    // Print the expansion List
+    print2DVector(expand);
 }
 
 int main()
