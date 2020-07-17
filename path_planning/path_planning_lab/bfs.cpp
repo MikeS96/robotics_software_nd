ad43314170f86a35f09854848b2c02127ad9608a
MikeS96 <miguel.saaruiz@gmail.com>
2020-07-17 14:33:08 -0500

    Created simple grid world in cpp

---
+++ path_planning/path_planning_lab/bfs.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Map class
+class Map {
+public:
+    const static int mapWidth = 6;
+    const static int mapHeight = 5;
+    vector<vector<int> > grid = {
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 1, 0, 0, 0, 0 },
+        { 0, 0, 0, 1, 1, 0 }
+    };
+};
+
+// Planner class
+class Planner : Map {
+public:
+    int start[2] = { 0, 0 };
+    int goal[2] = { mapHeight - 1, mapWidth - 1 };
+    int cost = 1;
+
+    string movements_arrows[4] = { "^", "<", "v", ">" };
+
+    vector<vector<int> > movements{
+        { -1, 0 },
+        { 0, -1 },
+        { 1, 0 },
+        { 0, 1 }
+    };
+};
+
+// Template function to print 2D vectors of any type
+template <typename T>
+void print2DVector(T Vec)
+{
+    for (int i = 0; i < Vec.size(); ++i) {
+        for (int j = 0; j < Vec[0].size(); ++j) {
+            cout << Vec[i][j] << ' ';
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    // Instantiate map and planner objects
+    Map map;
+    Planner planner;
+
+    // Print classes variables
+    cout << "Map:" << endl;
+    print2DVector(map.grid);
+    cout << "Start: " << planner.start[0] << " , " << planner.start[1] << endl;
+    cout << "Goal: " << planner.goal[0] << " , " << planner.goal[1] << endl;
+    cout << "Cost: " << planner.cost << endl;
+    cout << "Robot Movements: " << planner.movements_arrows[0] << " , " << planner.movements_arrows[1] << " , " << planner.movements_arrows[2] << " , " << planner.movements_arrows[3] << endl;
+    cout << "Delta:" << endl;
+    print2DVector(planner.movements);
+
+    return 0;
+}
